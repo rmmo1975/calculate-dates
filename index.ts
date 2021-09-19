c94cf8d518610ac259b355838ddc303fa694f984
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-09-19 02:48:29 -0300

    added parameter to inform a custom cutoff time; added validation for cutoff format;

--- index.ts
+++ index.ts
@@ -17,9 +17,18 @@ class CalculateDates {
    * @param {DaysOfTheWeek[]} possibleDaysOfTheWeekToSchedule - it can be one, some or all days of the week [ Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday ]
    * @param {string} timezone - valid timezone in USA based on tz_database
    * @param {Date | undefined} currentDatetime -  any valid datetime. if no valid date is informed, a new one will be create based on the current datetime
+   * @param {string | undefined} cutoffTime - time as string using the following format "hh:MM:ss:mmm" 24h format
    * @return {Date} returned the date found
    */
-  public static nextDate(possibleDaysOfTheWeekToSchedule: DaysOfTheWeek[], timezone: string, currentDatetime?: Date | undefined): Date | undefined {
+  public static nextDate(possibleDaysOfTheWeekToSchedule: DaysOfTheWeek[], timezone: string, currentDatetime?: Date, cutoffTime?: string): Date | undefined {
+
+    // check cutoffTime if used
+    if(cutoffTime) {
+      const timeRegex = /(?<time>(?<hours>[01]\d|2[0-3]):(?<minutes>[0-5]\d):(?<seconds>[0-5]\d)\.(?<milliseconds>\d{3}))$/mig;
+      if (!timeRegex.test(cutoffTime))
+        throw new Error('Invalid time format. Use this format "hh:MM:ss:mmm" 24h".');
+    }
+
     currentDatetime = currentDatetime || new Date();
     const pattern = 'yyyy-MM-dd HH:mm:ss.SSS'
     // get the date without timezone info (like GMT-0500). String representation of the current datetime
@@ -28,7 +37,7 @@ class CalculateDates {
     console.log(' -- current zoned datetime: ', zonedCurrentDatetime);
   
     // set the cutoff time of the current datetime
-    const zonedCutoffDatetime = `${zonedCurrentDatetime.substring(0, 10)} 02:00:00.000`;
+    const zonedCutoffDatetime = `${zonedCurrentDatetime.substring(0, 10)} ${cutoffTime || '00:00:00.000'}`;
     console.log(' -- cutoff datetime:     ', zonedCutoffDatetime);
   
     // generate a list of possible days to schedule 
@@ -52,10 +61,10 @@ class CalculateDates {
 
 export default { DaysOfTheWeek, CalculateDates };
 
-let myDate = CalculateDates.nextDate([DaysOfTheWeek.SATURDAY, DaysOfTheWeek.TUESDAY, DaysOfTheWeek.WEDNESDAY], 'America/Chicago', new Date('2021-09-15T03:00:00.000Z'));
+let myDate = CalculateDates.nextDate([DaysOfTheWeek.SATURDAY, DaysOfTheWeek.TUESDAY, DaysOfTheWeek.WEDNESDAY], 'America/Chicago', new Date('2021-09-15T03:00:00.000Z'), '02:00:00.000');
 console.log('date found: ', myDate);
 
-myDate = CalculateDates.nextDate([DaysOfTheWeek.SATURDAY, DaysOfTheWeek.TUESDAY, DaysOfTheWeek.WEDNESDAY], 'America/Los_Angeles', new Date('2021-09-15T03:00:00.000Z'));
+myDate = CalculateDates.nextDate([DaysOfTheWeek.SATURDAY, DaysOfTheWeek.TUESDAY, DaysOfTheWeek.WEDNESDAY], 'America/Los_Angeles', new Date('2021-09-15T03:00:00.000Z'), '02:00:00.000');
 console.log('date found: ', myDate);
 
 
